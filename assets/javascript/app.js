406759ccf5de3dbd161a21f959e8f8f1f5a657dd
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-10 20:55:26 -0500

    Got it set up so a start screen appears and when you click the start button, it goes away and is replaced by the quiz.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#start").on("click", function(){
+        console.log("I was clicked.");
+        $("#startScreen").addClass("disappear");
+        $("#quiz").removeClass("disappear");
+    });
+
+});
+
