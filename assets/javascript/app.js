e45ac573531aaf16a52bd72407f5ec3fc1875ff0
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-11 17:14:47 -0500

    got the countdown clock to work.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,23 @@
-var timeLeft;
-
+let timeLeft;
+let intervalId;
+let time = 10;
 
 $(document).ready(function(){
     $("#start").on("click", function(){
         console.log("I was clicked.");
         $("#startScreen").addClass("disappear");
         $("#quiz").removeClass("disappear");
+        clock();
     });
-
-    $()
-
 });
 
+function clock() {
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement () {
+    if (time > 0) {
+    time--;
+    $("#onscreenTimer").html(time);
+    }
+}
