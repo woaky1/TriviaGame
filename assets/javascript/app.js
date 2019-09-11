8938096ddb5b13120f75ce373429245fb43bcb7d
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-10 23:21:45 -0500

    working on how to check if a radio button question was answered correctly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,14 @@
+var timeLeft;
+
+
 $(document).ready(function(){
     $("#start").on("click", function(){
         console.log("I was clicked.");
         $("#startScreen").addClass("disappear");
         $("#quiz").removeClass("disappear");
     });
+
+    $()
 
 });
 
