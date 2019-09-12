be329ce3e262cb6cdd3f5525f7401a6960490392
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-11 21:29:03 -0500

    Found out how to loop through all the radio buttons and grab the checked value. Now does that and tallies correct, incorrect, and unanswered questions correctly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,11 @@ let intervalId;
 let time = 5;
 var radioValue;
 var graded = false;
+var userAnswers = [];
+var correctAnswers = []
+var numAnsweredCorrect = 0;
+var numAnsweredIncorrect = 0;
+var unansweredQuestions = 0;
 
 $(document).ready(function(){
     $("#start").on("click", function(){
@@ -28,14 +33,21 @@ function decrement() {
 }
 
 function grade() {
+// Learned how to loop through and grab the checked button values from one of the answers here: https://stackoverflow.com/questions/45848618/how-to-check-the-values-of-multiple-dynamic-radio-buttons-in-a-form-using-jquery
     if (graded === false) {
-        radioValue = $("input[name='question1']:checked").val();
-        console.log(radioValue);
-        if (radioValue === "correct"){
-            console.log("Correct");
-        } else {
-            console.log("Incorrect");
+        $('input[type=radio]:checked').each(function() {
+            userAnswers.push(this.value);
+            // console.log(userAnswers);
+        });
+        for(var i = 0; i < 2; i++) {
+            if (userAnswers[i] === "correct") {
+                numAnsweredCorrect++;
+            } else if (userAnswers[i] === "incorrect") {
+                numAnsweredIncorrect++;
+            }
         }
-    }
+        unansweredQuestions = 2 - userAnswers.length;
+        console.log(numAnsweredCorrect + "/" + numAnsweredIncorrect + "/" + unansweredQuestions);
+    };
     graded = true;
 }
